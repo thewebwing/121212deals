6ba11651b790ed5170513bcfee4249a3080f88ef
mike.rogne <mike@infinitywebcreations.com>
2012-11-12 22:17:33 -0600

    Set up MY_Controller usage in welcome controller.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,92 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+<h1>Welcome to CodeIgniter!</h1>
 
-	<style type="text/css">
-
-	::selection{ background-color: #E13300; color: white; }
-	::moz-selection{ background-color: #E13300; color: white; }
-	::webkit-selection{ background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body{
-		margin: 0 15px 0 15px;
-	}
+<div id="body">
+	<p>Congratulations! Your application has been kicked into gear and you're ready to start developing.</p>
+	<p>Don't forget, you'll want to change your encryption key! You can find it here:</p>
+	<code>application/config/config.php</code>
 	
-	p.footer{
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
+	<p>This starter-kit features several contributions from other developers and designers. You can find documentation for some below:</p>
 	
-	#container{
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		-webkit-box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
-</head>
-<body>
-
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>Congratulations! Your application has been kicked into gear and you're ready to start developing.</p>
-		<p>Don't forget, you'll want to change your encryption key! You can find it here:</p>
-		<code>application/config/config.php</code>
-		
-		<p>This starter-kit features several contributions from other developers and designers. You can find documentation for some below:</p>
-		
-		<p><a href="http://foundation.zurb.com/docs/">Zurb Foundation 3.2.2</a></p>
-		<p><a href="https://github.com/jamierumbelow/codeigniter-base-model">Jamie Rumbelow's MY_Model</a></p>
-		<p><a href="https://github.com/jamierumbelow/codeigniter-base-controller">Jamie Rumbelow's MY_Controller</a></p>
-		<p><a href="http://philsturgeon.co.uk/blog/2010/02/CodeIgniter-Base-Classes-Keeping-it-DRY">Base Classes by Phil Sturgeon</a></p>
-		<p>And of course, the wonderful <a href="http://codeigniter.com/user_guide/toc.html">CodeIgniter User Guide</a></p>
-		
-		<p>If you'd like, you can check out my company here: <a href="http://www.infinitywebcreations.com/">Infinity Web Creations - Expert Web Development</a>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+	<p><a href="http://foundation.zurb.com/docs/">Zurb Foundation 3.2.2</a></p>
+	<p><a href="https://github.com/jamierumbelow/codeigniter-base-model">Jamie Rumbelow's MY_Model</a></p>
+	<p><a href="https://github.com/jamierumbelow/codeigniter-base-controller">Jamie Rumbelow's MY_Controller</a></p>
+	<p><a href="http://philsturgeon.co.uk/blog/2010/02/CodeIgniter-Base-Classes-Keeping-it-DRY">Base Classes by Phil Sturgeon</a></p>
+	<p>And of course, the wonderful <a href="http://codeigniter.com/user_guide/toc.html">CodeIgniter User Guide</a></p>
+	
+	<p>If you'd like, you can check out my company here: <a href="http://www.infinitywebcreations.com/">Infinity Web Creations - Expert Web Development</a>
 </div>
 
-</body>
-</html>
+<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
